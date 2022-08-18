fa7892662ce9d11701a1a8e8fcbfdd30c63d29ef
Catherine_Fish <katerinasheps@gmail.com>
2022-08-18 12:35:38 +0300

    Added parser

---
+++ DrMemoryParser.py
@@ -0,0 +1,20 @@
+import re
+import os
+
+
+def parse(file_name):
+    res_flag = False
+    regex = re.compile(r'Error #1:')
+    with open(file_name) as file:
+        for line in file:
+            match = regex.match(line)
+            if match:
+                res_flag = True
+                break
+    return res_flag
+
+
+if __name__ == '__main__':
+    if parse('results.txt'):
+        os.system('type results.txt')
+        raise MemoryError('There are memory leaks in the code.')

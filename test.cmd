cae2424878cec459134627ea1227fbff3436d068
Catherine_Fish <katerinasheps@gmail.com>
2022-08-19 16:04:26 +0300

    Added smd

---
+++ test.cmd
@@ -0,0 +1,12 @@
+@echo off
+setlocal enabledelayedexpansion
+
+cd restest
+for /f %%i in ('"dir /s /b | find "results.txt""') do set var=%%i
+
+echo.%%var%%
+set var=%var:\=\\%
+echo.%%var%%
+
+cd ..
+copy %%var%% .\\

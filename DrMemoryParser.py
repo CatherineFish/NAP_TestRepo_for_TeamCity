8ebe82c1f410f582639434f4348b0720b9834ac6
Catherine_Fish <katerinasheps@gmail.com>
2022-08-19 13:43:04 +0300

    path

--- DrMemoryParser.py
+++ DrMemoryParser.py
@@ -15,7 +15,7 @@ def parse(file_name):
 
 
 if __name__ == '__main__':
-    with open('restest\file_name.txt') as file:
+    with open('filename.txt') as file:
         for line in file:
             if parse(line):
                 os.system('echo "There are memory leaks in the code."')

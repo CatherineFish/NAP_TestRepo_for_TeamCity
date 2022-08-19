473949e92cc457f56034ac2a1871cb736c047329
Catherine_Fish <katerinasheps@gmail.com>
2022-08-19 16:12:13 +0300

    Updated script

--- DrMemoryParser.py
+++ DrMemoryParser.py
@@ -13,10 +13,6 @@ def parse(file_name):
 
 
 if __name__ == '__main__':
-    with open('restest\\\\filename.txt') as file:
-        for line in file:
-            real_path = re.search("restest\\\\DrMemory-.*\\\\results.txt", line).group()  # making a relative path
-            if parse(real_path):
-                os.system('echo There are memory leaks in the code')  # keyword for an error in teamcity
-                os.system(f'type {real_path}')  # dr memory log to log
-                os.system(f'copy {real_path} .\\')  # for publication in artifacts
+    if parse("results.txt"):
+        os.system('echo There are memory leaks in the code')  # keyword for an error in teamcity
+        os.system(f'type results.txt')  # dr memory log to log

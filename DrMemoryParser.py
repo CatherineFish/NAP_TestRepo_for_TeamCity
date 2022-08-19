83a44eaa4a9cf4bf3b70c0d00b165f14f44a6d52
Catherine_Fish <katerinasheps@gmail.com>
2022-08-19 14:27:27 +0300

    parse filepath

--- DrMemoryParser.py
+++ DrMemoryParser.py
@@ -5,7 +5,6 @@ import os
 def parse(file_name):
     res_flag = False
     regex = re.compile(r'Error #1:')
-    print(file_name)
     with open(r"{}".format(file_name)) as file:
         for line in file:
             match = regex.match(line)
@@ -18,6 +17,7 @@ def parse(file_name):
 if __name__ == '__main__':
     with open('filename.txt') as file:
         for line in file:
-            if parse(line):
+            real_path = re.search("restest\\\\DrMemory-.*\\\\results.txt", line).group()
+            if parse(real_path):
                 os.system('echo "There are memory leaks in the code."')
-                os.system(f'type {line}')
+                os.system(f'type {real_path}')

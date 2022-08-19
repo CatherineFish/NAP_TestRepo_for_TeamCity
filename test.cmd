25aab4b0929332839ece894e607b78129b284e17
Catherine_Fish <katerinasheps@gmail.com>
2022-08-19 16:29:36 +0300

    Update test cmd

--- test.cmd
+++ test.cmd
@@ -4,9 +4,5 @@ setlocal enabledelayedexpansion
 cd restest
 for /f %%i in ('"dir /s /b | find "results.txt""') do set var=%%i
 
-echo.%var%
-set var=%var:\=\\%
-echo.%var%
-
 cd ..
-copy %var% .\\
+copy !var! .\\
